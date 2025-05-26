555c68fba671e35940745cd758e9ec7527bed006
PrimeTKL <anhtuan1442002@gmail.com>
2025-05-26 23:36:36 +0700

    chinh sua lai brand holder view

--- app/src/main/java/com/example/online_shop/ViewModel/MainViewModel.kt
+++ app/src/main/java/com/example/online_shop/ViewModel/MainViewModel.kt
@@ -9,7 +9,7 @@ import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ValueEventListener
-import kotlinx.serialization.builtins.ListSerializer
+
 
 class MainViewModel() : ViewModel() {
 
